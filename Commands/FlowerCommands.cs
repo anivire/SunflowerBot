f9c3dfa07b264304a85a5a494c429db4de3af4af
Nikita Grichan <56642689+aniv1re@users.noreply.github.com>
2020-06-20 22:18:51 +0300

    Deleted test commands

--- Commands/FlowerCommands.cs
+++ Commands/FlowerCommands.cs
@@ -65,5 +65,33 @@ namespace SunflowerBot.Commands
 
             await joinMessage.DeleteAsync().ConfigureAwait(false);
         }    
+
+        [Command("pool")]
+        [RequireRoles(RoleCheckMode.All, "Sun Sponsor")]
+        public async Task Poll(CommandContext ctx, TimeSpan duration, params DiscordEmoji[] emojiOptions)
+        {
+            var interactivity = ctx.Client.GetInteractivity();
+            var options = emojiOptions.Select(x => x.ToString());
+
+            var pollEmbed = new DiscordEmbedBuilder
+            {
+                Title = "Poll",
+                Description = string.Join(" ", options)
+            };
+
+            var pollMessage = await ctx.Channel.SendMessageAsync(embed: pollEmbed).ConfigureAwait(false);
+
+            foreach (var option in emojiOptions)
+            {
+                await pollMessage.CreateReactionAsync(option).ConfigureAwait(false);
+            }
+
+            var result = await interactivity.CollectReactionsAsync(pollMessage, duration).ConfigureAwait(false);
+            var distinctResult = result.Distinct();
+
+            var results = distinctResult.Select(x => $"{x.Emoji}: {x.Total}");
+
+            await ctx.Channel.SendMessageAsync(string.Join("\n", results)).ConfigureAwait(false);
+        }
     }
 }
