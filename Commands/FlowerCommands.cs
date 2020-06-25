b986fbdc8f8e46261850464c1d55fd5686531797
Nikita Grichan <56642689+aniv1re@users.noreply.github.com>
2020-06-25 23:15:46 +0300

    Add  LightJson parser, trying ti add users db

--- Commands/FlowerCommands.cs
+++ Commands/FlowerCommands.cs
@@ -1,4 +1,5 @@
 using SunflowerBot.Attributes;
+using LightJson;
 using DSharpPlus.CommandsNext;
 using DSharpPlus.EventArgs;
 using DSharpPlus.Commands​Next.Converters;
@@ -8,6 +9,7 @@ using DSharpPlus.Interactivity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace SunflowerBot.Commands
@@ -20,7 +22,7 @@ namespace SunflowerBot.Commands
         public async Task Sunny(CommandContext ctx)
         {
             Console.WriteLine($"[{DateTime.Now}] [Command Log] Использована команда .sunny пользователем {ctx.User.Username}");
-            var role = ctx.Guild.GetRole(724675912204812335);
+            var role = ctx.Guild.GetRole(720276705071333506);
 
             var interactivity = ctx.Client.GetInteractivity();
 
@@ -48,8 +50,8 @@ namespace SunflowerBot.Commands
                 await ctx.Member.GrantRoleAsync(role).ConfigureAwait(false);
                 Console.WriteLine($"[{DateTime.Now}] [Role Log] Добавление роли {role.Id} пользователю {ctx.User.Username}");
 
-                // await joinMessage.DeleteReactionAsync(accept, ctx.User).ConfigureAwait(false);
-            }           
+                // await joinMessage.DeleteReactionAsync(accept, ctx.User).ConfigureAwait(false);*/
+            }  
         }    
 
         [Command("give")]
@@ -233,6 +235,43 @@ namespace SunflowerBot.Commands
                 await joinMessage.CreateReactionAsync(DiscordEmoji.FromName(ctx.Client, ":upVote:")).ConfigureAwait(false);
                 await joinMessage.CreateReactionAsync(DiscordEmoji.FromName(ctx.Client, ":downVote:")).ConfigureAwait(false);
             }
+        }
+
+        [Command("reload")]
+        [Hidden]
+        [RequireRoles(RoleCheckMode.Any, "Sun Sponsor")]
+        public async Task Reload(CommandContext ctx)
+        {
+            var users = ctx.Guild.Members;
+
+            var sunCount = 0;
+
+            foreach (var item in users)
+            {
+                var tempUser = item.Value.Username;
+                var tempId = item.Key;
+
+                var profile = new JsonObject()
+	                .Add(tempUser, new JsonArray()
+                        .Add(tempUser)
+		                .Add(tempId.ToString())
+		                .Add(sunCount)
+	                )
+	                .ToString(true);
+
+                var user = JsonValue.Parse(profile)[tempUser].AsJsonArray;
+
+                foreach (var i in user)
+                {
+                    Console.WriteLine(i.ToString());
+                }
+
+                var path = Environment.CurrentDirectory + "\\usersData.json";
+
+                File.AppendAllText(path, profile + Environment.NewLine);
+            }
+
+            await ctx.Channel.SendMessageAsync("База данных была перезагружена.").ConfigureAwait(false);
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using DSharpPlus;
 
 namespace SunflowerBot
 {
